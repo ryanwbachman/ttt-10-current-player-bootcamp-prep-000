43b40e78c23ab2bcad880070226f2c0d56c4dc6e
Ryan <ryanwbachman@gmail.com>
2017-09-16 21:19:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,6 +9,6 @@ end
 
 def turn_count(board)
   count = 0
-  board.each{|position| if position == "X" || position == "O"; count+=1; end }
+  board.each { |position| if position == "X" || position == "O"; count+=1; end }
   count
 end
