461189c3b078e9c70ba01f723093bfdcd91d15b7
Ryan <ryanwbachman@gmail.com>
2017-09-16 21:07:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,12 @@
 def current_player(board)
   turn = turn_count(board)
-  if turn % 2 == 1
-    "O"
-  else
-    "X"
-  end
+
 end
 
 def turn_count(board)
-  board_state = board.join
-  board_state = board_state.strip
-  board_state.length
+  count = 0
+  board.each |position| do
+    if position == "X" or position == "O" {count += 1}
+  end
+  count
 end
